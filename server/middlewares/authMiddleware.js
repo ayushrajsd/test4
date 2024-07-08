d970b6feb2b8f7e59e310ef8b4c62cadd19f809a
Ayush <ayush.raj.sd@gmail.com>
2024-07-08 14:28:34 +0530

    first commit

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  console.log("auth middleware");
+  try {
+    console.log("req headers", req.headers.authorization);
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(" fetching token", token);
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("verified token", verifiedToken);
+    req.body.userId = verifiedToken.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ success: false, message: "Token Invalid" });
+  }
+};
+
+module.exports = auth;
